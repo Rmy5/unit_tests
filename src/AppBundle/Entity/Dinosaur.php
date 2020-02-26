4f8448bd528b88511d03ab93d57c985810d33fb7
Rmy5 <courteaud.remy@gmail.com>
2020-02-26 17:01:40 +0100

    vid 11

--- src/AppBundle/Entity/Dinosaur.php
+++ src/AppBundle/Entity/Dinosaur.php
@@ -29,6 +29,10 @@ class Dinosaur
      */
     private $isCarnivorous;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Enclosure", inversedBy="dinosaurs")
+     */
+    private $enclosure;
 
     public function __construct(string $genus = 'Unknown', bool $isCarnivorous = false)
     {
---
+++ src/AppBundle/Entity/Enclosure.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace AppBundle\Entity;
+
+
+use AppBundle\Exception\DinosaursAreRunningRampantException;
+use AppBundle\Exception\NotABuffetException;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="enclosure")
+ */
+class Enclosure
+{
+    /**
+     * @var Collection
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Dinosaur", mappedBy="enclosure", cascade={"persist"})
+     */
+    private $dinosaurs;
+
+    /**
+     * @var Collection|Security[]
+     * @ORM\OneToMany()
+     */
+    private $securities;
+
+    public function __construct()
+    {
+        $this->securities = new ArrayCollection();
+        $this->dinosaurs = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getDinosaurs(): Collection
+    {
+        return $this->dinosaurs;
+    }
+
+    public function addDinosaur(Dinosaur $dinosaur)
+    {
+        if ( ! $this->canAddDinosaur($dinosaur)) {
+            throw new NotABuffetException();
+        }
+
+        if ( ! $this->isSecurityActive()) {
+            throw new DinosaursAreRunningRampantException('Are you craaazy?!');
+        }
+
+        $this->dinosaurs[] = $dinosaur;
+    }
+
+    public function isSecurityActive(): bool
+    {
+        foreach ($this->securities as $security) {
+            if ($security->getIsActive()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function canAddDinosaur(Dinosaur $dinosaur): bool
+    {
+        return count($this->dinosaurs) === 0
+            || $this->dinosaurs->first()->isCarnivorous() === $dinosaur->isCarnivorous();
+    }
+}
